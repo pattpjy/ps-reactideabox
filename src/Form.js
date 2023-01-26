b5ec4ac7e3e66cf17a995805665836717751ca4a
Patt Sookmark <pijiya@sookmark.com>
2023-01-26 16:13:32 -0700

    Add project starter files

---
+++ src/Form.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./Form.css";
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      description: "",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  submitIdea = (event) => {
+    event.preventDefault(); // prevents the page from refreshing when the form submits
+    const newIdea = {
+      id: Date.now(),
+      ...this.state, // spreading in the title and description
+    };
+    this.props.addIdea(newIdea); // using the addIdea method from App that we passed as a prop to Form
+    this.clearInputs(); // invoking the method I wrote below to reset the inputs
+  };
+
+  clearInputs = () => {
+    this.setState({ title: "", description: "" });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          value={this.state.title}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          name="description"
+          value={this.state.description}
+        />
+        <button onClick={(event) => this.submitIdea(event)}>SUBMIT</button>
+        {/* //adding the event listener to the button */}
+      </form>
+    );
+  }
+}
+
+export default Form;
