8fbad416e483121bb76c8395ed7b49ea2d739723
Patt Sookmark <pijiya@sookmark.com>
2023-01-30 23:23:58 -0700

    Add comment button

--- src/Card.js
+++ src/Card.js
@@ -10,23 +10,29 @@ const Card = ({
   onStarIdea,
 }) => {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-      <div className="card-header">
-        <button
-          className={isStarred ? "fa-solid fa-heart" : "fa-light fa-heart"}
-          //maybe missing state where it check if staredIdea is equal to true
-          onClick={() => {
-            onStarIdea(id);
-          }}
-        ></button>
-        <button
-          className="fa-light fa-xmark"
-          onClick={() => onDeleteIdea(id)}
-        ></button>
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+
+        <div className="card-header">
+          <button
+            className={isStarred ? "fa-solid fa-heart" : "fa-light fa-heart"}
+            //maybe missing state where it check if staredIdea is equal to true
+            onClick={() => {
+              onStarIdea(id);
+            }}
+          ></button>
+          <button
+            className="fa-light fa-xmark"
+            onClick={() => onDeleteIdea(id)}
+          ></button>
+        </div>
+        <button className="comment-btn">
+          <i class="fa-light fa-comment-dots"></i> comment
+        </button>
       </div>
     </div>
   );
